d91a1a0f9b42886a12edb84c10432f9dd273a0da
wkn816 <wkn816@gmail.com>
2020-11-30 23:01:47 +0900

    カウントアップ呼び出し

---
+++ components/counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h2>{{ title}}</h2>
+    counter: {{ $store.state.counter.counter}}
+    <button v-on:click="$store.commit('counter/countUp')">+1</button>
+  </section>
+</template>
+
+<script>
+export default {
+  date: function(){
+    title: 'Countup app'
+  }
+}
+</script>
